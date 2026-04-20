a8ff26f27d793636f1cfaa5a86ee65bcd82d13ec
angelaguirrem34-rgb <angel.aguirrem34@my.unitec.edu.mx>
2026-04-20 17:39:50 -0600

    test Application

---
+++ test/Application.IntegrationTest/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿namespace Application.IntegrationTest;
+
+public class UnitTest1
+{
+    [Theory]
+        [InlineData(6, 5, 7, 454.65)] // Polígono regular de 6 lados, lado=5, altura=7
+        public void CalcularVolumen_PoligonoRegular_Correcto(int lados, double lado, double altura, double expected)
+        {
+            // Arrange: calculamos el área del polígono regular
+            double area = (lados * Math.Pow(lado, 2)) / (4 * Math.Tan(Math.PI / lados));
+
+            // Act: calculamos el volumen como área * altura
+            double volumen = area * altura;
+
+            // Assert: verificamos que el resultado sea el esperado (con tolerancia)
+            Assert.Equal(expected, volumen, 2); // tolerancia de 2 decimales
+        }
+}
